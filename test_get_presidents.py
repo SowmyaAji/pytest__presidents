4abca907a020dd126abb433d91941bf74cb2fb25
sowmya aji <sowmya.aji@gmail.com>
2022-11-05 22:15:24 -0400

    Remove unnecessary comments

--- test_get_presidents.py
+++ test_get_presidents.py
@@ -1,7 +1,3 @@
-# Write a PyTest module that queries the DuckDuckGo api for “presidents of the united states,” and tests that each president is listed in the response.  (If you need a list or our presidents, see Presidents (opens in new window))
-# The presidents should be listed in the RelatedTopics returned field.  RelatedTopics is a list, and you should check the Text entries of RelatedTopics to search for presidents.  We’ll only look for the last name of a president.  That means that we won’t distinguish between John Adams and his son, John Quincy Adams, or George Bush the senior and ‘W.’
-# Place your test code in a GitHub repository.  
-
 import pytest
 from get_presidents import QueryPresidents
 
